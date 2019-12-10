12c4f1bf66af46a886a6936895a9cd79e896beff
MACorman <madelinecorman@gmail.com>
2019-12-10 16:26:26 -0500

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,9 @@
+class Puppy
+    attr_accessor :name, :breed, :age
+
+    def initialize(params)
+        @name = params[:name]
+        @breed = params[:breed]
+        @age = params[:age]
+    end
+end
